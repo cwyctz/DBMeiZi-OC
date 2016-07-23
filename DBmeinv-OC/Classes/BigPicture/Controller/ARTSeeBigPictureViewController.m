1e41f7b31968bcf0888d85e4d581167a5700c734
a455455b <Artillery@macbook-pro.local>
2016-07-23 22:49:46 +0800

    增加了保存图片功能

--- DBmeinv-OC/Classes/BigPicture/Controller/ARTSeeBigPictureViewController.m
+++ DBmeinv-OC/Classes/BigPicture/Controller/ARTSeeBigPictureViewController.m
@@ -10,7 +10,12 @@
 #import <UIImageView+WebCache.h>
 #import "ARTImageItems.h"
 #import "UIView+Frame.h"
+#import <Photos/Photos.h>
+#import <SVProgressHUD.h>
+
 @interface ARTSeeBigPictureViewController ()
+
+@property (nonatomic,weak) UIImageView *imageView;
 
 @end
 
@@ -47,10 +52,67 @@
     
     }];
 
-
+    self.imageView = img;
     
 }
 - (IBAction)SaveBtnClick:(UIButton *)sender {
+    
+    __block NSString *createdAssetId = nil;
+    // 添加图片到【相机胶卷】
+    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+        createdAssetId = [PHAssetChangeRequest creationRequestForAssetFromImage:self.imageView.image].placeholderForCreatedAsset.localIdentifier;
+    } error:nil];
+    
+    // 在保存完毕后取出图片
+    PHFetchResult<PHAsset *> *createdAssets = [PHAsset fetchAssetsWithLocalIdentifiers:@[createdAssetId] options:nil];
+    
+    // 获取软件的名字作为相册的标题
+    NSString *title = [NSBundle mainBundle].infoDictionary[(NSString *)kCFBundleNameKey];
+    
+    // 已经创建的自定义相册
+    PHAssetCollection *createdCollection = nil;
+    
+    // 获得所有的自定义相册
+    PHFetchResult<PHAssetCollection *> *collections = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+    for (PHAssetCollection *collection in collections) {
+        if ([collection.localizedTitle isEqualToString:title]) {
+            createdCollection = collection;
+            break;
+        }
+    }
+    
+    if (!createdCollection) { // 没有创建过相册
+        __block NSString *createdCollectionId = nil;
+        // 创建一个新的相册
+        [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+            createdCollectionId = [PHAssetCollectionChangeRequest creationRequestForAssetCollectionWithTitle:title].placeholderForCreatedAssetCollection.localIdentifier;
+        } error:nil];
+        
+        // 创建完毕后再取出相册
+        createdCollection = [PHAssetCollection fetchAssetCollectionsWithLocalIdentifiers:@[createdCollectionId] options:nil].firstObject;
+    }
+    
+    if (createdAssets == nil || createdCollection == nil) {
+        [SVProgressHUD showErrorWithStatus:@"保存失败！"];
+        return;
+    }
+    
+    // 将刚才添加到【相机胶卷】的图片，引用（添加）到【自定义相册】
+    NSError *error = nil;
+    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+        PHAssetCollectionChangeRequest *request = [PHAssetCollectionChangeRequest changeRequestForAssetCollection:createdCollection];
+        [request insertAssets:createdAssets atIndexes:[NSIndexSet indexSetWithIndex:0]];
+    } error:&error];
+    
+    // 保存结果
+    if (error) {
+        [SVProgressHUD showErrorWithStatus:@"保存失败！"];
+    } else {
+        [SVProgressHUD showSuccessWithStatus:@"保存成功！"];
+    }
+
+
+    
 }
 
 - (IBAction)backBtnClick:(UIButton *)sender
